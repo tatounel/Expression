7e4e8624d63f704c87c55a233332b0c19dc1b224
tatounel <tatou.nel@gmail.com>
2019-11-26 19:42:01 -0500

    stuff i've done

--- profile.js
+++ profile.js
@@ -7,12 +7,71 @@ import {
   Dimensions,
   TextInput,
   TouchableOpacity,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Picker,
+  SafeAreaView
 } from "react-native";
 import Image from "react-native-scalable-image";
+import MultiSelect from "react-native-multiple-select";
+
+this.items = [];
 
 export default class createProfileScreen extends React.Component {
+  state = {
+    //We will store selected item in this
+    selectedItems: [],
+    items: [],
+    success: true,
+    error: false
+  };
+  componentDidMount() {
+    this.getGenres();
+  }
+  onSelectedItemsChange = selectedItems => {
+    this.setState({ selectedItems });
+    //Set Selected Items
+  };
+
+  getGenres = () => {
+    fetch(`http://localhost:8000/api/genres/`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        //console.log(`Got genres: ${res}`);
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Content validation");
+      })
+      .then(genres => {
+        this.setState({
+          success: true
+        });
+        console.log(genres);
+        let items = [];
+        genreNames = genres.filter(genre => {
+          let obj = genre;
+          let itemElement = { id: `${obj.id}`, name: `${obj.name}` };
+          console.log(obj.name);
+          items.push(itemElement);
+        });
+        this.setState({
+          items: items
+        });
+      })
+      .catch(err => {
+        this.setState({
+          error: true
+        });
+      });
+  };
   render() {
+    const { selectedItems } = this.state;
+    console.log(this.items);
     return (
       <KeyboardAvoidingView
         behavior="padding"
@@ -38,7 +97,7 @@ export default class createProfileScreen extends React.Component {
               style={styleCreateProfile.textProfileInputs}
             />
 
-            <TextInput
+            {/* <TextInput
               placeholder="Interest"
               placeholderTextColor="rgba(255,255,255,0.7)"
               returnKeyType="next"
@@ -46,7 +105,34 @@ export default class createProfileScreen extends React.Component {
               keyboardType="email-address"
               style={styleCreateProfile.textProfileInputs}
               ref={interests => (this.interestInput = interests)}
-            />
+            /> */}
+            <SafeAreaView style={{ flex: 1 }}>
+              <View style={{ flex: 1 }}>
+                <MultiSelect
+                  // hideTags
+                  items={this.state.items}
+                  uniqueKey="id"
+                  ref={component => {
+                    this.multiSelect = component;
+                  }}
+                  onSelectedItemsChange={this.onSelectedItemsChange}
+                  selectedItems={selectedItems}
+                  selectText="Pick Interests"
+                  searchInputPlaceholderText="Search Items..."
+                  onChangeInput={text => console.log(text)}
+                  tagRemoveIconColor="#CCC"
+                  tagBorderColor="#CCC"
+                  tagTextColor="#CCC"
+                  selectedItemTextColor="#CCC"
+                  selectedItemIconColor="#CCC"
+                  itemTextColor="#000"
+                  displayKey="name"
+                  searchInputStyle={{ color: "#CCC" }}
+                  submitButtonColor="#48d22b"
+                  submitButtonText="Submit"
+                />
+              </View>
+            </SafeAreaView>
 
             <TextInput
               placeholder="Bio"
