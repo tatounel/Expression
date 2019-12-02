b362d9d39cfc97bf895c2987cfb7e35217937e8e
Rebecca-CYLi <48325436+Rebecca-CYLi@users.noreply.github.com>
2019-12-01 23:14:21 -0500

    authentications
    added authentications

--- profile.js
+++ profile.js
@@ -1,27 +1,66 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import React from 'react';
+import { SafeAreaView, StyleSheet, Image, 
+        TouchableOpacity, AsyncStorage, Text } from 'react-native';
+import User from './User';
 
-class profileScreen extends React.Component{
-    render(){
-        return(
-            <View style = {StyleProfile.container}>
-                <Text>Please Create Your Profile</Text>
-                <Image style = {{width: 300, height: 200}}
-                source = {require("./assets/xpression.png")}/>
-                
 
-            </View>
-        );
+import * as firebase from "firebase";
+
+export default class ProfileScreen extends React.Component {
+    //For Top of page Details
+    static navigationOptions = ({ navigation }) => {
+        return{ 
+          title: 'Chat_Profile',
+        }
+      }
+
+    state = {
+        emal:"",
+        password: "",
+    };
+    componentDidMount(){
+        const {email, password} = firebase.auth().currentUser;
+        this.setState({email, password});
+    }
+    signOut = () => {
+        firebase.auth().signOut();
+    }
+    //logout Button
+    logOut = async () => {
+        await AsyncStorage.clear();
+        this.props.navigation.navigate('Auth');
+    }
+
+    render() {
+        return (
+            <SafeAreaView style={profileStyle.container}>
+                <Text style={{ fontSize: 20 }}>
+                    {/* {User.email} */}
+                    Hi {this.state.email}!
+                </Text>
+
+                <Text style={{ fontSize: 20 }}>
+                    {/* {User.name} */}
+                    Hi {this.state.null}
+                </Text>
+
+                {/* <TouchableOpacity onPress={this.logOut}> */}
+                <TouchableOpacity onPress={this.signOut}>
+                    <Text style = {profileStyle.logout}>Logout</Text>
+                </TouchableOpacity>
+
+            </SafeAreaView>
+        )
     }
 }
 
-export default profileScreen;
-
-const StyleProfile = StyleSheet.create({
+const profileStyle = StyleSheet.create({
     container:{
-        flex: 1,
-        backgroundColor: "#ff0080",
-        alignItems: "center",
-        justifyContent: "center",
+        flex: 1
     },
+    logout: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: 'blue'
+    }
 })
