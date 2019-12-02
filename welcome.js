6723f8979f5f6e489e7684eac50009dad37b7ee0
Rebecca-CYLi <48325436+Rebecca-CYLi@users.noreply.github.com>
2019-12-01 23:16:19 -0500

    from local
    local branch updates

--- welcome.js
+++ welcome.js
@@ -1,35 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, StyleSheet, 
+        TouchableOpacity } from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
+//import Image from "react-native-scalable-image";
 
-//building a class that includes the touchability to move forward to the next page for each button
-
+/* building a class that includes the touchability to move forward 
+    to the next page for each button */
 class WelcomeScreen extends React.Component {
+
+  //For Top Page Details
+  static navigationOptions = ({ navigation }) => {
+    return{ 
+      title: 'Welcome',
+    }
+  }   
+    
     render(){
         return(
             <View style = {styleWelcome.container}>
                 <Text>Welcome To</Text>
                 <Image style = {{width: 300, height: 200}}
-                source = {require("./assets/xpression.png")}/>
+                    source = {require("./assets/xpression.png")}/>
                 <Text>Where Artists and Authors Unite</Text>
 
                     <TouchableOpacity style = {styleWelcome.buttoncontainer} >
                     <View style = {styleWelcome.button1}>
-                    <AwesomeButton
-                    textColor= "#000000"
-                    backgroundColor= "#5ce1e6"
-                    onPress = {() => this.props.navigation.navigate('Login')}
-                    >Log In
-                    </AwesomeButton>
+                        <AwesomeButton
+                        textColor= "#000000"
+                        backgroundColor= "#5ce1e6"
+                        onPress = {() => this.props.navigation.navigate('Login')}
+                        >Log In
+                        </AwesomeButton>
                     </View>
                     
                     <View>
-                    <AwesomeButton
-                    textColor= "#000000"
-                    backgroundColor= "#5ce1e6"
-                    onPress = {() => this.props.navigation.navigate('Signup')}
-                    >Sign Up
-                    </AwesomeButton>
+                        <AwesomeButton
+                        textColor= "#000000"
+                        backgroundColor= "#5ce1e6"
+                        onPress = {() => this.props.navigation.navigate('Signup')}
+                        >Sign Up
+                        </AwesomeButton>
                     </View>
                     </TouchableOpacity>
             </View>
