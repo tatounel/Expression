f8529980a2c93d1d69be367457d505aeecd56da8
Rebecca-CYLi <48325436+Rebecca-CYLi@users.noreply.github.com>
2019-12-11 23:40:26 -0500

    UPDATED CHATAPP FUNCTIONS

--- App.js
+++ App.js
@@ -15,14 +15,7 @@ import chatScreen from "./chatScreens/chatScreen";
 import ProfileScreen from "./chatScreens/ProfileScreen";
 import HomeScreen from "./chatScreens/homeScreen.js";
 
-// CHAT APP #Latest
-// import Boiler from "./chatScreens/Boiler";
-// import Chat from "./chatScreens/Chat";
-// import friendList from "./chatScreens/friendList";
-// import globalChat from "./chatScreens/globalChat";
-
 import LoadingScreen from "./chatScreens/LoadingScreen";
-// /* YT authentication tutorial https://www.youtube.com/watch?v=TkuQAjnaSbM&t=128s */
 import * as firebase from 'firebase';
 // // Your web app's Firebase configuration
 var firebaseConfig = {
@@ -41,10 +34,6 @@ firebase.initializeApp(firebaseConfig);
 const AppStack = createStackNavigator({
   Home: HomeScreen,
   Chat: chatScreen,
-  // Boiler: Boiler,
-  // Global: globalChat,
-  // FriendList: friendList,
-  // Chat: Chat,
   //FIRST PAGE HERE IS THE PAGE YOU SEE AFTER SIGNUP/LOGIN
   Profile: ProfileScreen,
   EditProfile: createProfileScreen,
@@ -73,50 +62,3 @@ export default createAppContainer(
     }
   )
 );
-
-//CHAT #Latest 
-// class Home extends React.Component<{}> {
-//   constructor(props){
-//     super();
-//     this.state = {
-//       page: "connection",
-//       loading: "true",
-//       authenticated: false
-//     };
-//   }
-//   static navigationOptions = {
-//     headerStyle: {
-//       backgroundColor: "#16a085",
-//       elevation: null
-//     },
-//     header: null
-//   };
-//   componentDidMount(){
-//     firebase.auth().onAuthStateChanged(user => {
-//       if(user) {
-//         this.setState({ loading: false, authenticated: true});
-//       }else{
-//         this.setState({ loading: false, authenticated: false});
-//       }
-//     });
-//   }
-//   render(){
-//     if(this.state.loading) return null;
-//     if(!this.state.authenticated){
-//       return <Login navigation = {this.props.navigation} />;
-//     }
-//     return <Boiler navigation = {this.props.navigation} />;
-//   }
-// }
-
-// export default App = StackNavigation({
-//   Home: {
-//     screen: Home
-//   },
-//   Login: {
-//     screen: loginScreen
-//   },
-//   Signup: {
-//     screen: signUpScreen
-//   }
-// })
