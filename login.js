bb2e6c42a5769b5adc7692615e36ebf162b0c494
Rebecca Li <Rebecca.CYLi@gmail.com>
2019-12-13 16:55:24 -0500

    test chat

--- login.js
+++ login.js
@@ -1,48 +1,157 @@
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Image, KeyboardAvoidingView, StatusBar } from "react-native";
+import { StyleSheet, Text, View, Image, 
+        TextInput, KeyboardAvoidingView, 
+        StatusBar, AsyncStorage, Alert,
+        TouchableOpacity } from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
+
+//import firebase from "firebase";
+import User from './User';
+import firebase from "firebase";
 
 //Building a login that includes inputs of each placeholder needed to be filled out
 class loginScreen extends React.Component {
+    //For Top Page Details
+    static navigationOptions = ({ navigation }) => {
+        return{ 
+          title: 'Log In',
+        }
+      }
+  
+    state = {
+      email: "",
+      username: "",
+      password: "",
+      name: "",
+      errormsg: null
+    }
+   
+
+  
+    //FOR CHAT APP
+    handleChange = key => val => {
+      this.setState({ [key] : val})
+    }
+    componentWillMount(){
+      AsyncStorage.getItem('email').then(val => {
+        if(val){
+          this.setState({ email : val })
+        }
+      })
+    }
+    componentWillUnmount(){
+    }
+
+    submitForm = async() => {
+      const{email, username, password, name} = this.state;
+      if(this.state.email.length < 3){
+        Alert.alert('Error', 'Wrong Email Address')
+      }else if(this.state.username.length < 2){
+        Alert.alert('Error', 'Wrong name!')
+      }else{             
+        //this is for User.js?
+        await AsyncStorage.setItem('email', email);
+        await AsyncStorage.setItem('username', username);
+        await AsyncStorage.setItem('password', password);
+        await AsyncStorage.setItem('name', name);
+        User.name = this.state.name;
+        User.email = this.state.email;
+        User.password = this.state.password;
+        User.username = this.state.username;
+        //console.log(User.username+ " TESTING")
+        firebase
+          .auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+          .then(()=>{
+            //this.readUserData();
+            //WORKS FOR A SIGNED'UP ACC
+            this.props.nagivation.navigate('App');
+            this.setState({error: "", loading: false});
+          })
+          .catch(()=>{
+            this.setState({error: "Login Authentication failed.", loading: false})
+        });
+      }//end of else
+    }
+  readUserData() {
+      firebase.database().ref('Users/').once('value', function (snapshot) {
+          console.log(snapshot.val())
+      });
+  }//reads signed up user account details
+
     render(){
         return(
             <KeyboardAvoidingView behavior = "padding" style = {styleLogin.container}>
                 <View style = {styleLogin.container}>
-                <StatusBar 
-                 barStyle = "light-content"
-                />
+                <StatusBar barStyle = "light-content" />
                 <Text>Welcome To</Text>
                 <Image style = {{width: 300, height: 200}}
-                source = {require("./assets/xpression.png")}/>
+                    source = {require("./assets/xpression.png")}/>
                 <Text>Where Artists and Authors Unite</Text>
                 
                 <View style = {styleLogin.loginContainer}>
                     <TextInput
-                    placeholder ="E-mail or Username"
-                    placeholderTextColor = "rgba(255,255,255,0.7)"
-                    returnKeyType = "next"
-                    onSubmitEditing={() => this.passwordInput.focus()}
-                    keyboardType = "email-address"
-                    autoCapitalize ="none"
-                    autoCorrect={false}
-                    style ={styleLogin.textInput}
+                        placeholder ="E-mail"
+                        placeholderTextColor = "rgba(255,255,255,0.7)"
+                        returnKeyType = "next"
+                        onSubmitEditing={() => this.passwordInput.focus()}
+                        keyboardType = "email-address"
+                        autoCapitalize ="none"
+                        autoCorrect={false}
+                        style ={styleLogin.textInput}
+
+                        //for chatScreen
+                        value={this.state.email}
+                        onChangeText={this.handleChange('email')}
+                        // value={this.state.username}
+                        // onChangeText={this.handleChange('username')}
                     />
 
                     <TextInput 
-                    placeholder = "Password"
-                    placeholderTextColor = "rgba(255,255,255,0.7)"
-                    returnKeyType = "go"
-                    secureTextEntry
-                    style = {styleLogin.textInput}
-                    ref ={(input) => this.passwordInput = input}
+                        placeholder = "Password"
+                        placeholderTextColor = "rgba(255,255,255,0.7)"
+                        returnKeyType = "go"
+                        secureTextEntry
+                        style = {styleLogin.textInput}
+                        //refer to onsubmitediting, this helps move onto the next input
+                        ref ={(input) => this.passwordInput = input}
+
+                        //for chatScreen
+                        value={this.state.password}
+                        onChangeText={this.handleChange('password')}
                     />
+
+                    {/* TEST FOR FIREBASE CHAT APP */}
+                    <TextInput
+                        placeholder = "Username"
+                        placeholderTextColor = "rgba(255,255,255,0.7)"
+                        returnKeyType = "next"
+                        keyboardType = "email-address"
+                        autoCapitalize ="none"
+                        autoCorrect={false}
+                        style ={styleLogin.textInput}
+
+                        //for chatScreen
+                        value={this.state.username}
+                        onChangeText={this.handleChange('username')}
+                    />
+
                 </View>
 
                 <AwesomeButton
                     textColor= "#000000"
                     backgroundColor= "#5ce1e6"
+                    // onPress = {this.handleLogin, () => 
+                    //   this.props.navigation.navigate("DisplayProfile")}
+                    //   onPress = {this.handleLogin, this.submitForm}
+                      onPress = {this.submitForm}
+                    // onPress={this.onLoginPress.bind(this)}
                     >Login
                 </AwesomeButton>
+
+                <View style={styleLogin.errorMessage}>
+                    {this.state.errorMessage && <Text style={styleLogin.error}>{this.state.errorMessage}</Text>}
+                </View>
+
             </View>
             </KeyboardAvoidingView>
 
@@ -70,6 +179,18 @@ const styleLogin = StyleSheet.create({
         marginBottom: 20,
         color: '#FFF',
         paddingHorizontal: 10
-    }
+    },
+    errorMessage: {
+      //height: 72,
+      alignItems: "center",
+      justifyContent: "center",
+      //marginHorizontal: 30
+  },
+  error: {
+      color: "#E9446A",
+      fontSize: 13,
+      fontWeight: "600",
+      textAlign: "center"
+  },
         
 });
