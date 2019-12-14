19547dfb679a089ae23051f84faa611b63ea60af
tatounel <tatou.nel@gmail.com>
2019-12-14 15:02:14 -0500

    chat app

--- chatScreens/homeScreen.js
+++ chatScreens/homeScreen.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, 
-        TextInput, StatusBar, FlatList,
-        KeyboardAvoidingView, AsyncStorage,
-        TouchableOpacity, SafeAreaView, Dimensions } from "react-native";
-import User from '../User';
-import firebase from 'firebase';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  StatusBar,
+  FlatList,
+  KeyboardAvoidingView,
+  AsyncStorage,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions
+} from "react-native";
+import User from "../User";
+import firebase from "firebase";
 import AwesomeButton from "react-native-really-awesome-button";
 
 // export default class HomeScreen extends React.Component{
 //     static navigationOptions = {
 //         header: null
 //     }
-    
+
 //     state = {
 //         users: [],
 //         dbRef: firebase.database().ref('users')
@@ -33,7 +43,6 @@ import AwesomeButton from "react-native-really-awesome-button";
 //         }
 //     }
 
-
 //     signOut = async () => {
 //         await AsyncStorage.clear();
 //         this.props.navigation.navigate('Auth');
@@ -53,10 +62,10 @@ import AwesomeButton from "react-native-really-awesome-button";
 //         return(
 //             <SafeAreaView>
 
-//                 <FlatList 
+//                 <FlatList
 //                     data={this.state.users}
 //                     renderItem={this.renderRow}
-//                     keyExtractor={(item) => item.email} 
+//                     keyExtractor={(item) => item.email}
 //                     ListHeaderComponent = { () => <Text> Chat List </Text>} />
 
 //                 <TouchableOpacity onPress={this.signOut}>
@@ -75,7 +84,7 @@ import AwesomeButton from "react-native-really-awesome-button";
 //     // render(){
 //     //     return(
 //     //         <View style={styles.container}>
-//     //             <View style = {styles.circle} /> 
+//     //             <View style = {styles.circle} />
 
 //     //             <View style={{ marginHorizontal: 32}}>
 //     //                 <Text style={styles.header}>Username</Text>
@@ -85,121 +94,120 @@ import AwesomeButton from "react-native-really-awesome-button";
 //     // }//end of render
 // }
 
-export default class HomeScreen extends React.Component{
-    static navigationOptions = {
-        title: 'Chat List'
-    }
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: "Chat List"
+  };
 
-    state = {
-        users: [],
-        dbRef: firebase.database().ref('Users')
-    }
-    componentDidMount(){
-        let dbRef = firebase.database().ref('Users');
-        this.state.dbRef.on('child_added', (val)=> {
-            let person = val.val();
-            person.email = val.key;
-                this.setState((prevState)=> {
-                    return {
-                        users: [...prevState.users, person]
-                    }
-                })
-            
-        })
-    }
-    componentWillUnmount(){
-        this.state.dbRef.off();
-    }
+  state = {
+    users: [],
+    dbRef: firebase.database().ref("Users")
+  };
+  componentDidMount() {
+    let dbRef = firebase.database().ref("Users");
+    this.state.dbRef.on("child_added", val => {
+      let person = val.val();
+      person.email = val.key;
+      this.setState(prevState => {
+        return {
+          users: [...prevState.users, person]
+        };
+      });
+    });
+  }
+  componentWillUnmount() {
+    this.state.dbRef.off();
+  }
 
-    signOut = async () => {
-        await AsyncStorage.clear();
-        this.props.navigation.navigate('Auth');
-    }
-    renderRow = ({item}) => {
-        return(
-            <TouchableOpacity 
-                onPress={ () => this.props.navigation.navigate('Chat', item)}
-                style={{ padding:10, borderBottomColor:'#ccc', borderBottomWidth: 1}} > 
-                <Text style = {{fontSize: 20}}> {item.username} </Text>
-            </TouchableOpacity>
-        )
-    }
-    render(){
-        return(
-            <SafeAreaView style = {home.backgrd}>
-                <FlatList 
-                    ListHeaderComponent = { () => <Text style={home.listStart}> Users Chat List </Text>} 
-                    data={this.state.users} 
-                    renderItem={this.renderRow}
-                    keyExtractor={(item) => item.email} 
-                />
+  signOut = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Welcome");
+  };
+  renderRow = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => this.props.navigation.navigate("ChatScreen", item)}
+        style={{ padding: 10, borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      >
+        <Text style={{ fontSize: 20 }}> {item.username} </Text>
+      </TouchableOpacity>
+    );
+  };
+  render() {
+    return (
+      <SafeAreaView style={home.backgrd}>
+        <FlatList
+          ListHeaderComponent={() => (
+            <Text style={home.listStart}> Users Chat List </Text>
+          )}
+          data={this.state.users}
+          renderItem={this.renderRow}
+          keyExtractor={item => item.email}
+        />
 
-                {/* <TouchableOpacity onPress={this.signOut}>
+        {/* <TouchableOpacity onPress={this.signOut}>
                     <Text style = {home.signout}> Logout </Text>
                 </TouchableOpacity> */}
 
-            <View style = {home.signout} >
-                <AwesomeButton
-                textColor= "#000000"
-                backgroundColor= "#5ce1e6"
-                onPress = {() => this.props.navigation.navigate('DisplayProfile')}
-                >Go to Profile
-                </AwesomeButton>
+        <View style={home.signout}>
+          <AwesomeButton
+            textColor="#000000"
+            backgroundColor="#5ce1e6"
+            onPress={() => this.props.navigation.navigate("DisplayProfile")}
+          >
+            Go to Profile
+          </AwesomeButton>
 
-
-
-                <TouchableOpacity >
-                    <View style = {home.signout}>
-                        <AwesomeButton
-                            textColor= "#000000"
-                            backgroundColor= "#5ce1e6"
-                            onPress={this.signOut}
-                        >Logout
-                        </AwesomeButton>
-                    </View>
-                </TouchableOpacity>
-                
-                </View>
-
-
-            </SafeAreaView>
-        )
-    }
+          <TouchableOpacity>
+            <View style={home.signout}>
+              <AwesomeButton
+                textColor="#000000"
+                backgroundColor="#5ce1e6"
+                onPress={this.signOut}
+              >
+                Logout
+              </AwesomeButton>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 const home = StyleSheet.create({
-    backgrd:{
-        backgroundColor: "#f285b2"
-    },
-    container: {
-        flex: 1,
-        backgroundColor: "#F4F5F7"
-    },
-    circle:{
-        width: 500,
-        height: 500,
-        borderRadius: 500/2,
-        backgroundColor: "#FFFFFF",
-        position: "absolute",
-        left: -120,
-        top: -20
-    },
-    signout: {
-        textAlign: "center",
-        fontSize: 20,
-        color: 'blue',
-        margin: 20,
-        alignItems: "center",
-        flexDirection: "row",
-        justifyContent: "center"
-    },
-    welcome: {
-        textAlign: "center",
-        fontSize: 20,
-        color: "green",
-        margin: 10
-    },
-    listStart: {
-        textAlign: "center",
-        margin: 10
-    }
-})
+  backgrd: {
+    backgroundColor: "#f285b2"
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#F4F5F7"
+  },
+  circle: {
+    width: 500,
+    height: 500,
+    borderRadius: 500 / 2,
+    backgroundColor: "#FFFFFF",
+    position: "absolute",
+    left: -120,
+    top: -20
+  },
+  signout: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "blue",
+    margin: 20,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  welcome: {
+    textAlign: "center",
+    fontSize: 20,
+    color: "green",
+    margin: 10
+  },
+  listStart: {
+    textAlign: "center",
+    margin: 10
+  }
+});
--- login.js
+++ login.js
@@ -1,196 +1,211 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, 
-        TextInput, KeyboardAvoidingView, 
-        StatusBar, AsyncStorage, Alert,
-        TouchableOpacity } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  KeyboardAvoidingView,
+  StatusBar,
+  AsyncStorage,
+  Alert,
+  TouchableOpacity
+} from "react-native";
 import AwesomeButton from "react-native-really-awesome-button";
 
 //import firebase from "firebase";
-import User from './User';
+import User from "./User";
 import firebase from "firebase";
 
 //Building a login that includes inputs of each placeholder needed to be filled out
 class loginScreen extends React.Component {
-    //For Top Page Details
-    static navigationOptions = ({ navigation }) => {
-        return{ 
-          title: 'Log In',
-        }
+  //For Top Page Details
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "Log In"
+    };
+  };
+
+  state = {
+    email: "",
+    username: "",
+    password: "",
+    name: "",
+    errormsg: null
+  };
+
+  //FOR CHAT APP
+  handleChange = key => val => {
+    this.setState({ [key]: val });
+  };
+  componentWillMount() {
+    AsyncStorage.getItem("email").then(val => {
+      if (val) {
+        this.setState({ email: val });
       }
-  
-    state = {
-      email: "",
-      username: "",
-      password: "",
-      name: "",
-      errormsg: null
-    }
-   
+    });
+  }
+  componentWillUnmount() {}
 
-  
-    //FOR CHAT APP
-    handleChange = key => val => {
-      this.setState({ [key] : val})
-    }
-    componentWillMount(){
-      AsyncStorage.getItem('email').then(val => {
-        if(val){
-          this.setState({ email : val })
-        }
-      })
-    }
-    componentWillUnmount(){
-    }
-
-    submitForm = async() => {
-      const{email, username, password, name} = this.state;
-      if(this.state.email.length < 3){
-        Alert.alert('Error', 'Wrong Email Address')
-      }else if(this.state.username.length < 2){
-        Alert.alert('Error', 'Wrong name!')
-      }else{             
-        //this is for User.js?
-        await AsyncStorage.setItem('email', email);
-        await AsyncStorage.setItem('username', username);
-        await AsyncStorage.setItem('password', password);
-        await AsyncStorage.setItem('name', name);
-        User.name = this.state.name;
-        User.email = this.state.email;
-        User.password = this.state.password;
-        User.username = this.state.username;
-        //console.log(User.username+ " TESTING")
-        firebase
-          .auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-          .then(()=>{
-            //this.readUserData();
-            //WORKS FOR A SIGNED'UP ACC
-            this.props.nagivation.navigate('App');
-            this.setState({error: "", loading: false});
-          })
-          .catch(()=>{
-            this.setState({error: "Login Authentication failed.", loading: false})
+  submitForm = async () => {
+    const { email, username, password, name } = this.state;
+    if (this.state.email.length < 3) {
+      Alert.alert("Error", "Wrong Email Address");
+    } else if (this.state.username.length < 2) {
+      Alert.alert("Error", "Wrong name!");
+    } else {
+      //this is for User.js?
+      await AsyncStorage.setItem("email", email);
+      await AsyncStorage.setItem("username", username);
+      await AsyncStorage.setItem("password", password);
+      await AsyncStorage.setItem("name", name);
+      User.name = this.state.name;
+      User.email = this.state.email;
+      User.password = this.state.password;
+      User.username = this.state.username;
+      //console.log(User.username+ " TESTING")
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(() => {
+          //this.readUserData();
+          //WORKS FOR A SIGNED'UP ACC
+          // this.props.nagivation.navigate('App');
+          this.setState({ error: "", loading: false });
+        })
+        .catch(() => {
+          this.setState({
+            error: "Login Authentication failed.",
+            loading: false
+          });
         });
-      }//end of else
-    }
+    } //end of else
+  };
   readUserData() {
-      firebase.database().ref('Users/').once('value', function (snapshot) {
-          console.log(snapshot.val())
+    firebase
+      .database()
+      .ref("Users/")
+      .once("value", function(snapshot) {
+        console.log(snapshot.val());
       });
-  }//reads signed up user account details
+  } //reads signed up user account details
 
-    render(){
-        return(
-            <KeyboardAvoidingView behavior = "padding" style = {styleLogin.container}>
-                <View style = {styleLogin.container}>
-                <StatusBar barStyle = "light-content" />
-                <Text>Welcome To</Text>
-                <Image style = {{width: 300, height: 200}}
-                    source = {require("./assets/xpression.png")}/>
-                <Text>Where Artists and Authors Unite</Text>
-                
-                <View style = {styleLogin.loginContainer}>
-                    <TextInput
-                        placeholder ="E-mail"
-                        placeholderTextColor = "rgba(255,255,255,0.7)"
-                        returnKeyType = "next"
-                        onSubmitEditing={() => this.passwordInput.focus()}
-                        keyboardType = "email-address"
-                        autoCapitalize ="none"
-                        autoCorrect={false}
-                        style ={styleLogin.textInput}
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styleLogin.container}>
+        <View style={styleLogin.container}>
+          <StatusBar barStyle="light-content" />
+          <Text>Welcome To</Text>
+          <Image
+            style={{ width: 300, height: 200 }}
+            source={require("./assets/xpression.png")}
+          />
+          <Text>Where Artists and Authors Unite</Text>
 
-                        //for chatScreen
-                        value={this.state.email}
-                        onChangeText={this.handleChange('email')}
-                        // value={this.state.username}
-                        // onChangeText={this.handleChange('username')}
-                    />
+          <View style={styleLogin.loginContainer}>
+            <TextInput
+              placeholder="E-mail"
+              placeholderTextColor="rgba(255,255,255,0.7)"
+              returnKeyType="next"
+              onSubmitEditing={() => this.passwordInput.focus()}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styleLogin.textInput}
+              //for chatScreen
+              value={this.state.email}
+              onChangeText={this.handleChange("email")}
+              // value={this.state.username}
+              // onChangeText={this.handleChange('username')}
+            />
 
-                    <TextInput 
-                        placeholder = "Password"
-                        placeholderTextColor = "rgba(255,255,255,0.7)"
-                        returnKeyType = "go"
-                        secureTextEntry
-                        style = {styleLogin.textInput}
-                        //refer to onsubmitediting, this helps move onto the next input
-                        ref ={(input) => this.passwordInput = input}
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor="rgba(255,255,255,0.7)"
+              returnKeyType="go"
+              secureTextEntry
+              style={styleLogin.textInput}
+              //refer to onsubmitediting, this helps move onto the next input
+              ref={input => (this.passwordInput = input)}
+              //for chatScreen
+              value={this.state.password}
+              onChangeText={this.handleChange("password")}
+            />
 
-                        //for chatScreen
-                        value={this.state.password}
-                        onChangeText={this.handleChange('password')}
-                    />
+            {/* TEST FOR FIREBASE CHAT APP */}
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor="rgba(255,255,255,0.7)"
+              returnKeyType="next"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styleLogin.textInput}
+              //for chatScreen
+              value={this.state.username}
+              onChangeText={this.handleChange("username")}
+            />
+          </View>
 
-                    {/* TEST FOR FIREBASE CHAT APP */}
-                    <TextInput
-                        placeholder = "Username"
-                        placeholderTextColor = "rgba(255,255,255,0.7)"
-                        returnKeyType = "next"
-                        keyboardType = "email-address"
-                        autoCapitalize ="none"
-                        autoCorrect={false}
-                        style ={styleLogin.textInput}
+          <AwesomeButton
+            textColor="#000000"
+            backgroundColor="#5ce1e6"
+            // onPress = {this.handleLogin, () =>
+            //   this.props.navigation.navigate("DisplayProfile")}
+            //   onPress = {this.handleLogin, this.submitForm}
+            onPress={
+              (this.submitForm,
+              () => {
+                this.props.navigation.navigate("ChatHomeScreen");
+              })
+            }
+            // onPress={this.onLoginPress.bind(this)}
+          >
+            Login
+          </AwesomeButton>
 
-                        //for chatScreen
-                        value={this.state.username}
-                        onChangeText={this.handleChange('username')}
-                    />
-
-                </View>
-
-                <AwesomeButton
-                    textColor= "#000000"
-                    backgroundColor= "#5ce1e6"
-                    // onPress = {this.handleLogin, () => 
-                    //   this.props.navigation.navigate("DisplayProfile")}
-                    //   onPress = {this.handleLogin, this.submitForm}
-                      onPress = {this.submitForm}
-                    // onPress={this.onLoginPress.bind(this)}
-                    >Login
-                </AwesomeButton>
-
-                <View style={styleLogin.errorMessage}>
-                    {this.state.errorMessage && <Text style={styleLogin.error}>{this.state.errorMessage}</Text>}
-                </View>
-
-            </View>
-            </KeyboardAvoidingView>
-
-        );
-    }
+          <View style={styleLogin.errorMessage}>
+            {this.state.errorMessage && (
+              <Text style={styleLogin.error}>{this.state.errorMessage}</Text>
+            )}
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 export default loginScreen;
 
 const styleLogin = StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "#ff0080",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#ff0080",
+    alignItems: "center",
+    justifyContent: "center"
+  },
 
-    loginContainer:{
-        padding: 20
-    },
+  loginContainer: {
+    padding: 20
+  },
 
-    textInput: {
-        height: 40,
-        backgroundColor: 'rgba(255,255,255,0.2)',
-        marginBottom: 20,
-        color: '#FFF',
-        paddingHorizontal: 10
-    },
-    errorMessage: {
-      //height: 72,
-      alignItems: "center",
-      justifyContent: "center",
-      //marginHorizontal: 30
+  textInput: {
+    height: 40,
+    backgroundColor: "rgba(255,255,255,0.2)",
+    marginBottom: 20,
+    color: "#FFF",
+    paddingHorizontal: 10
+  },
+  errorMessage: {
+    //height: 72,
+    alignItems: "center",
+    justifyContent: "center"
+    //marginHorizontal: 30
   },
   error: {
-      color: "#E9446A",
-      fontSize: 13,
-      fontWeight: "600",
-      textAlign: "center"
-  },
-        
+    color: "#E9446A",
+    fontSize: 13,
+    fontWeight: "600",
+    textAlign: "center"
+  }
 });
